0808d8ebde7f87ea5897d692a6267eef857c8d9b
vaibhava03 <99650989+vaibhava03@users.noreply.github.com>
2022-02-24 10:32:34 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,57 @@
+const http = require('http');
+// const routes=require('./routes');
+// routes.
+const fs=require('fs'); 
+var message=fs.createWriteStream('message.txt');
+const server=http.createServer((req, res) =>
+{
+  const url=req.url;
+  const method=req.method; 
+  if(url==='/')
+{
+  res.write('<html>');
+  res.write('<head><title>Enter Message</title></head>');
+  res.write(`<body><p>${fs.readFileSync('message.txt')}</p><form action ="/message" method ="POST"> <input type ="text" name ="message"> <button type="submit">Send</button></body>`);
+  res.write('</html>');
+  return res.end();
+}
+if(url === '/message' && method === 'POST')
+{
+    const body=[];
+    req.on('data', (chunk) =>
+    {
+        console.log(chunk);
+        body.push(chunk);
+    });
+    req.on('end',() =>
+    {
+      parsedBody=Buffer.concat(body).toString();
+        message.write(parsedBody.split('=')[1]);
+       message.write('  ');
+  });
+
+  res.statusCode=302;
+       res.setHeader('Location', '/');
+       return res.end();
+}
+res.setHeader('Content-Type', 'text/html');
+ res.write('<html>');
+ res.write('<head><title>My First Page</title></head>');
+ res.write('<body><h3>Hello from Node js</h3></body>'); 
+ res.write('</html>');
+ return res.end();
+});
+server.listen(4000);
+
+// if(req.url==='/home')
+//   {
+//       res.write('<p>Welcome Home</p>');
+//   }
+//   if(req.url==='/about')
+//   {
+//       res.write('<p>Welcome to About Us page</p>');
+//   }
+//   if(req.url==='/node')
+//   {
+//       res.write('<p>Welcome to my Node js project</p>');
+//   }
