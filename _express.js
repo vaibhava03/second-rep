5f7df59bc6bdbbd789c06180cefcc5eb6e708d33
vaibhava03 <99650989+vaibhava03@users.noreply.github.com>
2022-02-25 11:24:54 +0530

    Update _express.js

--- _express.js
+++ _express.js
@@ -1,15 +1,29 @@
 
-const express=require('express');
-const app=express();
-app.use((req, res, next) =>{
-    console.log('In middleware');
-    next();
-});
-app.use((req, res, next) =>{
-    console.log('In anothermiddleware');
-    res.send("<h1>hello from express!</h1>");
 
-});
+    const express=require('express');
 
-app.listen(3000);
+    const app=express();
+
+    const bodyParser=require('body-parser');
+
+    app.use(bodyParser.urlencoded({extended:false}));
+
+    
+    app.use('/add-product', (req, res, next) =>{
+       
+        res.send('<form action ="/product" method="POST">\<input type: "text" name="title" placeholder="add product"> <input type: "number" name="size" placeholder="add size"><button type="submit">submit</button></form>');
+
+        });
+    
+    app.post('/product',(req, res, next) =>{
+        console.log(req.body);
+        res.redirect('/');
+        });
+    
+    app.use('/', (req, res, next) =>{
+        
+        res.send( '<h1>hello from express!</h1>');
+        });
+    
+    app.listen(3000);
 
